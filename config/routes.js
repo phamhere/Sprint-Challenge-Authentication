cd5aed1950e358a04ef5e6bbdb19c8f01ed0e49c
Peter Pham <phamhere@gmail.com>
2018-11-30 09:25:09 -0700

    added dependencies and register function

--- config/routes.js
+++ config/routes.js
@@ -1,15 +1,23 @@
-const axios = require('axios');
+const axios = require("axios");
+const bcrypt = require("bcryptjs");
 
-const { authenticate } = require('./middlewares');
+const { authenticate } = require("./middlewares");
 
 module.exports = server => {
-  server.post('/api/register', register);
-  server.post('/api/login', login);
-  server.get('/api/jokes', authenticate, getJokes);
+  server.post("/api/register", register);
+  server.post("/api/login", login);
+  server.get("/api/jokes", authenticate, getJokes);
 };
 
 function register(req, res) {
   // implement user registration
+  const creds = req.body;
+  const hash = bcrypt.hashSync(creds.password, 14);
+  creds.password = hash;
+  db("users")
+    .insert(creds)
+    .then(id => res.status(201).json(id))
+    .catch(err => res.status(500).json(err));
 }
 
 function login(req, res) {
@@ -19,12 +27,12 @@ function login(req, res) {
 function getJokes(req, res) {
   axios
     .get(
-      'https://08ad1pao69.execute-api.us-east-1.amazonaws.com/dev/random_ten'
+      "https://08ad1pao69.execute-api.us-east-1.amazonaws.com/dev/random_ten"
     )
     .then(response => {
       res.status(200).json(response.data);
     })
     .catch(err => {
-      res.status(500).json({ message: 'Error Fetching Jokes', error: err });
+      res.status(500).json({ message: "Error Fetching Jokes", error: err });
     });
 }
